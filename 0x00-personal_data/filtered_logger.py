eac0481b826c14d883f31af04dc225f641742c8e
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-05-19 20:02:57 +0300

    personal_data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -18,11 +18,14 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: ls):
+        """constructor"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        return filter_datum(self.fields, self.REDACTION, super().format(record), self.SEPARATOR)
+        """formatting"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
 
 
 def filter_datum(fields: ls, redaction: s, message: s, separator: s) -> str:
