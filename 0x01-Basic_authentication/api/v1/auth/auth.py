c36c00e0f02c09213e9a5aaa09e1bda2b9fb58ac
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-05-21 01:11:42 +0300

    Basic_authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,11 @@ User = TypeVar('User')
 
 
 class Auth:
+    """
+    Auth Class
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """class method"""
         if (
             path is None or excluded_paths is None or len(excluded_paths) < 1
             or all(path != excluded_path
@@ -21,7 +25,9 @@ class Auth:
         return False
 
     def authorization_header(self, request=None) -> str:
+        """class method"""
         return None
 
     def current_user(self, request=None) -> User:
+        """class method"""
         return None
