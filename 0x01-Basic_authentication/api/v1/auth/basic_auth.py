03dbd0da6f8de290dc6415b1259a6789464aa179
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-05-21 19:41:39 +0300

    Basic_authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -46,8 +46,6 @@ class BasicAuth(Auth):
     def user_object_from_credentials(
             self, user_email: str, user_pwd: str) -> TypeVar('User'):
         """function to get user_object"""
-        if user_email is None or user_pwd is None:
-            return None
         if type(user_email) != str or type(user_pwd) != str:
             return None
         users = User.search({'email': user_email})
