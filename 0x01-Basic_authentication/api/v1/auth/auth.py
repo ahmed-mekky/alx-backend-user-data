d9cfa04b6702d4debd8b5b9d043897327b260efe
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-05-21 06:06:52 +0300

    Basic_authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,14 +15,14 @@ class Auth:
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """class method"""
+        if path is None or excluded_paths is None or len(excluded_paths) < 1:
+            return True
         for excluded_path in excluded_paths:
             if excluded_path[-1] == '*':
                 if excluded_path[:-1] in path:
                     return False
             elif path != excluded_path and path != excluded_path[:-1]:
                 return True
-        if path is None or excluded_paths is None or len(excluded_paths) < 1:
-            return True
         return False
 
     def authorization_header(self, request=None) -> str:
