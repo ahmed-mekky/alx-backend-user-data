5f4010c55bd2da5a30c7aac7fc710c92231552a6
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-05-21 05:23:57 +0300

    Basic_authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@ Module for the BasicAuth class
 """
 from .auth import Auth
 import base64
+from typing import Tuple, TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -12,11 +14,8 @@ class BasicAuth(Auth):
             self,
             authorization_header: str) -> str:
         """extracts base64 header"""
-        try:
-            auth_header = str(authorization_header).split()
-            if not authorization_header or auth_header[0] != 'Basic':
-                return None
-        except ValueError:
+        auth_header = str(authorization_header).split()
+        if type(authorization_header) != str or auth_header[0] != 'Basic':
             return None
         return auth_header[1]
 
@@ -24,11 +23,34 @@ class BasicAuth(Auth):
             self,
             base64_authorization_header: str) -> str:
         """decode base64 header"""
-        if not base64_authorization_header:
+        if type(base64_authorization_header) != str:
             return None
         try:
             base64_header = base64.b64decode(
-                str(base64_authorization_header), validate=True)
+                base64_authorization_header, validate=True)
         except Exception:
             return None
         return base64_header.decode('utf-8')
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str) -> Tuple[str, str]:
+        """function to extract_user_credentials"""
+        if type(decoded_base64_authorization_header) != str:
+            return (None, None)
+        base64_header = str(decoded_base64_authorization_header).split(':')
+        if len(base64_header) < 2:
+            return (None, None)
+        return (base64_header[0], base64_header[1])
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """function to get user_object"""
+        if type(user_email) != str or type(user_pwd) != str or not User.all():
+            return None
+        users = User.search({'email': user_email})
+        if users:
+            user = users[0]
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
