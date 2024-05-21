d5df31ec2bcef667f6a69f4ded6f139f7c100605
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-05-21 05:49:04 +0300

    Basic_authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -54,3 +54,10 @@ class BasicAuth(Auth):
             if user.is_valid_password(user_pwd):
                 return user
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        header = self.authorization_header(request)
+        extracted = self.extract_base64_authorization_header(header)
+        decoded = self.decode_base64_authorization_header(extracted)
+        email, pwd = self.extract_user_credentials(decoded)
+        return self.user_object_from_credentials(email, pwd)
