00715b172448dcaed6ecb075f02a7e918e3df77d
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-05-22 15:30:45 +0300

    Basic_authentication

--- 0x01-Basic_authentication/models/base.py
+++ 0x01-Basic_authentication/models/base.py
@@ -123,7 +123,6 @@ class Base():
 
     @classmethod
     def search(cls, attributes: dict = {}) -> List[TypeVar('Base')]:
-        print(DATA)
         """ Search all objects with matching attributes
         """
         s_class = cls.__name__
