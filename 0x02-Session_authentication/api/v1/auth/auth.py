56642fe28e436c5218bc827db71efb66d7c3fc9b
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-05-24 01:26:39 +0300

    Session_authentication

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Module for the Auth class
+"""
+from typing import List, TypeVar
+
+
+User = TypeVar('User')
+
+
+class Auth:
+    """
+    Auth Class
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """class method"""
+        if path is None or excluded_paths is None or len(excluded_paths) < 1:
+            return True
+        for excluded_path in excluded_paths:
+            if excluded_path[-1] == '*':
+                if excluded_path[:-1] in path:
+                    return False
+            if path in excluded_path:
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """class method"""
+        if request is None:
+            return None
+        header = request.headers.get('Authorization')
+        if not header:
+            return None
+        return header
+
+    def current_user(self, request=None) -> User:
+        """class method"""
+        return None
---
+++ 0x02-Session_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""
+Module for the BasicAuth class
+"""
+from .auth import Auth
+import base64
+from typing import Tuple, TypeVar
+from models.user import User
+
+
+class BasicAuth(Auth):
+    """Basic auth class"""
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
+        """extracts base64 header"""
+        auth_header = str(authorization_header).split()
+        if type(authorization_header) != str or auth_header[0] != 'Basic':
+            return None
+        return auth_header[1]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """decode base64 header"""
+        if type(base64_authorization_header) != str:
+            return None
+        try:
+            base64_header = base64.b64decode(
+                base64_authorization_header, validate=True)
+        except Exception:
+            return None
+        return base64_header.decode('utf-8')
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str) -> Tuple[str, str]:
+        """function to extract_user_credentials"""
+        if type(decoded_base64_authorization_header) != str:
+            return (None, None)
+        base64_header = str(decoded_base64_authorization_header).split(':', 1)
+        if len(base64_header) < 2:
+            return (None, None)
+        return (base64_header[0], base64_header[1])
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """function to get user_object"""
+        if type(user_email) != str or type(user_pwd) != str:
+            return None
+        users = User.search({'email': user_email})
+        if users:
+            for user in users:
+                if user.is_valid_password(user_pwd):
+                    return user
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """returns a user from a request"""
+        header = self.authorization_header(request)
+        extracted = self.extract_base64_authorization_header(header)
+        decoded = self.decode_base64_authorization_header(extracted)
+        email, pwd = self.extract_user_credentials(decoded)
+        return self.user_object_from_credentials(email, pwd)
