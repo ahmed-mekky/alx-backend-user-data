4a836eacea78fdafa228f65d55235bcf6bd47e18
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-05-24 01:49:32 +0300

    Session Auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -10,8 +10,14 @@ class SessionAuth (Auth):
     """Basic auth class"""
     user_id_by_session_id = {}
     def create_session(self, user_id: str = None) -> str:
+        """creates a session"""
         if not isinstance(user_id, str):
             return None
         session_id = uuid4()
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """returns the in-session user id"""
+        if not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
