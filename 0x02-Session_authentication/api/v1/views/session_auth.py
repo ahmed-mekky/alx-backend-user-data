4cfa16f466883e4299dd3c1f53de548187035efe
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-05-24 03:01:59 +0300

    Session Auth

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -19,7 +19,7 @@ def login() -> str:
         return jsonify({"error": "email missing"}), 400
     if not password :
         return jsonify({"error": "password missing"}), 400
-    user = User.search({'email': email})
+    user = User.search({'email': email})[0]
     if not user:
         return jsonify({"error": "no user found for this email"}), 404
     if not user.is_valid_password(password):
--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -28,10 +28,10 @@ def view_one_user(user_id: str = None) -> str:
     if user_id is None:
         abort(404)
     if user_id == 'me':
-        print(request.current_user)
         if request.current_user is None:
             abort(404)
         user = request.current_user
+        print(user)
     else:
         user = User.get(user_id)
     if user is None:
