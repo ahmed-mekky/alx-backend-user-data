591f375dd1dcad4aaf239d5e0e023b32b9382a47
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-06-01 01:23:45 +0300

    user_authentication_service

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """auth stuff"""
-from bcrypt import hashpw, gensalt
+from bcrypt import hashpw, gensalt, checkpw
 from db import DB
 from user import User
 
@@ -20,6 +20,14 @@ class Auth:
         except Exception:
             return self._db.add_user(email, _hash_password(password))
         raise ValueError(f"User {email} already exists")
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """check user's password"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except Exception:
+            return False
+        return checkpw(password.encode(), user.hashed_password)
 
 
 def _hash_password(password: str) -> bytes:
