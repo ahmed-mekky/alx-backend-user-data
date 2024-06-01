dbc6845a444a01bfcd16ae4edcefd798d9b58d2a
Ahmed <ahmed.mekkawi.74@gmail.com>
2024-06-01 05:00:42 +0300

    User Auth Service

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """flask app"""
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 
@@ -36,6 +36,18 @@ def login():
         return response
 
     return abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'])
+def logout():
+    session_id = request.cookies.get('session_id')
+
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        return abort(403)
+    AUTH.destroy_session(session_id)
+    return redirect('/sessions')
+
 
 
 if __name__ == "__main__":
